82675deed3833b7050fd8ba25aba299e004d24e0
Tatiana Betancur <tatianabetancur27dev@hotmail.com>
2019-09-25 23:06:57 -0500

    Test streams terminal gnu

---
+++ test_streams_terminal_linux.py
@@ -0,0 +1,20 @@
+# python3 test_streams_terminal_linux.py 1> salida 2> error
+# tail -f salida, tail -f salida
+# python3 test_streams_terminal_linux.py 1>> salida 2>> error
+
+import time
+import sys
+
+def main():
+    i = 1
+    num = int(input('Escribe un número: '))
+    while(True):
+        if(i % num == 0):
+            print(f'El {i} es múltiplo de {num}, todo cool', flush=True)
+        else:
+            print(f'NO!! ALARMA: El {i} NO es múltiplo de {num}', file=sys.stderr, flush=True)
+        i += 1
+        time.sleep(1)
+
+if __name__ == "__main__":
+    main()
